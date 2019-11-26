34047f7913708788bc327b5f3e5598e1a1b11c57
Ziyi Ye <ziy116@ucsd.edu>
2019-11-25 23:22:12 -0800

    data cleaning for length

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-import pandas as import pd
 
 
-# initialize list of lists
-data = [['tom', 10], ['nick', 15], ['juli', 14]]
+def standardize_length(input):
+    hour = 0
+    min = 0
+    sec = 0
 
-# Create the pandas DataFrame
-df = pd.DataFrame(data, columns=['Name', 'Age'])
+    input = input.replace('PT', '')
+    input = input.upper()
+    if 'H' in input:
+        hour = input.split('H')[0]
+        input = input.split('H')[1]
+    if 'M' in input:
+        min = input.split('M')[0]
+        input = input.split('M')[1]
+    if 'S' in input:
+        sec = input.split('S')[0]
+        input = input.split('S')[1]
+    return hour, min, sec
 
-df[0:1]
+
+test = ['PT1H2M3S', 'PT1H3S', 'PT2M3S', 'PT1H3S', 'PT3S', 'PT2M', 'PT1H']
+
+for s in test:
+    result = standardize_length(s)
+    print(result[0], 'H', result[1], 'M', result[2], 'S')
